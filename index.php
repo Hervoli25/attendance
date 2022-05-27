c3f6c7889cdde612cf92bc0ca23c7480e66d69d3
Herve Tshombe <daddyhwhynot@gmail.com>
2022-05-27 08:14:19 +0200

    ready

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $title = 'Index';
 require_once 'includes/header.php';
-require_once 'includes/connec.php';
+require_once 'dbase/conn.php';
+
 
 ?>
 
